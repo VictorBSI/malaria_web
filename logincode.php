a489b5dda77e04dcc6f7eeb8a666332e651e40a3
victorbsi <victorhugo543af@gmail.com>
2023-02-22 16:46:45 -0300

    Add new Features

--- logincode.php
+++ logincode.php
@@ -5,22 +5,32 @@
         $email = $_POST['email'];
         $pass = $_POST['pass'];
 
-        $query = "SELECT * FROM malaria.login WHERE email LIKE '$email' and pass LIKE '$pass'";
+/*$query = "SELECT * FROM malaria.login WHERE email LIKE '$email'"; // and pass LIKE '$pass'
+    $res = mysqli_query($conn, $query);
+    $data = mysqli_fetch_array($res);
+
+    if(isset($data[0]) ? 2 : 1 > 1){
+        if(password_verify($pass, $data["pass"])){
+            echo json_encode("true");
+        } else {
+            echo json_encode("false");
+        }
+    } else {
+        echo json_encode("dont have an account");
+    }*/ 
+
+        $query = "SELECT * FROM malaria.login WHERE email LIKE '$email'";
         $res = mysqli_query($conn, $query);
         $data = mysqli_fetch_array($res);
 
-        if(isset($data[0]) ? 2 : 1 > 1){
-            //echo json_encode("account already exists");
-            $query = "SELECT * FROM malaria.login WHERE pass LIKE '$pass'";
-            $res = mysqli_query($conn, $query);
-            $data = mysqli_fetch_array($res);
-
-            if($data[2] >= 1){
+        if(password_verify($pass, $data["pass"])){
+            header("Location: index.php");
+            /*if($data[2] >= 1){
                 echo json_encode("true");
                 header("Location: index.php");
             }else{
                 echo json_encode("false");
-            }
+            }*/
 
         } else {
             echo json_encode("dont have an account");
